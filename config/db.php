da1eb8429f0842db5adc8a39f03e22095e438b27
zxj <656861622@qq.com>
2017-07-18 14:46:23 +0800

    修复一些问题

--- config/db.php
+++ config/db.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Container\Container;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
 
 $config = @include dirname(__DIR__).'/../Application/Common/Conf/config.php';
 
@@ -18,9 +20,6 @@ $capsule->addConnection([
 ]);
 
 // Set the event dispatcher used by Eloquent models... (optional)
-use Illuminate\Database\Capsule\Manager as Capsule;
-use Illuminate\Events\Dispatcher;
-
 $capsule->setEventDispatcher(new Dispatcher(new Container()));
 
 // Make this Capsule instance available globally via static methods... (optional)
